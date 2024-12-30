d5c0bef5a534c6f42b9dfa1567d2894ae32f826a
HG_Wang <cnhgwang@gmail.com>
2024-12-30 08:04:01 +0000

    修正-正常运行

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -8,7 +8,7 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
   
-/** See [8254] for hardware details of the 8254 timer chip. */
+/* See [8254] for hardware details of the 8254 timer chip. */
 
 #if TIMER_FREQ < 19
 #error 8254 timer requires TIMER_FREQ >= 19
@@ -17,10 +17,10 @@
 #error TIMER_FREQ <= 1000 recommended
 #endif
 
-/** Number of timer ticks since OS booted. */
+/* Number of timer ticks since OS booted. */
 static int64_t ticks;
 
-/** Number of loops per timer tick.
+/* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
@@ -30,19 +30,16 @@ static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
-/** Sets up the timer to interrupt TIMER_FREQ times per second,
+/* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
 timer_init (void) 
 {
-  //PIT: 可编程间隔定时器, channel 0, mode 2, frequency TIMER_FREQ(100 次/秒)
-  //给PIT配置配置特定的频率,模式和channel，这样PIT就会以这个配置产生中断
   pit_configure_channel (0, 2, TIMER_FREQ);
-  //时钟中断注册: 对应的是起始中断向量号0x20, 中断处理函数是timer_interrupt, 名字是"8254 Timer"
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
 
-/** Calibrates loops_per_tick, used to implement brief delays. */
+/* Calibrates loops_per_tick, used to implement brief delays. */
 void
 timer_calibrate (void) 
 {
@@ -69,7 +66,7 @@ timer_calibrate (void)
   printf ("%'"PRIu64" loops/s.\n", (uint64_t) loops_per_tick * TIMER_FREQ);
 }
 
-/** Returns the number of timer ticks since the OS booted. */
+/* Returns the number of timer ticks since the OS booted. */
 int64_t
 timer_ticks (void) 
 {
@@ -79,7 +76,7 @@ timer_ticks (void)
   return t;
 }
 
-/** Returns the number of timer ticks elapsed since THEN, which
+/* Returns the number of timer ticks elapsed since THEN, which
    should be a value once returned by timer_ticks(). */
 int64_t
 timer_elapsed (int64_t then) 
@@ -87,23 +84,19 @@ timer_elapsed (int64_t then)
   return timer_ticks () - then;
 }
 
-/** Sleeps for approximately TICKS timer ticks.  Interrupts must
+/* Sleeps for approximately TICKS timer ticks.  Interrupts must
    be turned on. */
 void
 timer_sleep (int64_t ticks) 
 {
-  /*
-    int64_t start = timer_ticks ();
+  int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
     thread_yield ();
-  */
-  thread_sleep(ticks); //调用thread_sleep函数
-
 }
 
-/** Sleeps for approximately MS milliseconds.  Interrupts must be
+/* Sleeps for approximately MS milliseconds.  Interrupts must be
    turned on. */
 void
 timer_msleep (int64_t ms) 
@@ -111,7 +104,7 @@ timer_msleep (int64_t ms)
   real_time_sleep (ms, 1000);
 }
 
-/** Sleeps for approximately US microseconds.  Interrupts must be
+/* Sleeps for approximately US microseconds.  Interrupts must be
    turned on. */
 void
 timer_usleep (int64_t us) 
@@ -119,7 +112,7 @@ timer_usleep (int64_t us)
   real_time_sleep (us, 1000 * 1000);
 }
 
-/** Sleeps for approximately NS nanoseconds.  Interrupts must be
+/* Sleeps for approximately NS nanoseconds.  Interrupts must be
    turned on. */
 void
 timer_nsleep (int64_t ns) 
@@ -127,7 +120,7 @@ timer_nsleep (int64_t ns)
   real_time_sleep (ns, 1000 * 1000 * 1000);
 }
 
-/** Busy-waits for approximately MS milliseconds.  Interrupts need
+/* Busy-waits for approximately MS milliseconds.  Interrupts need
    not be turned on.
 
    Busy waiting wastes CPU cycles, and busy waiting with
@@ -140,7 +133,7 @@ timer_mdelay (int64_t ms)
   real_time_delay (ms, 1000);
 }
 
-/** Sleeps for approximately US microseconds.  Interrupts need not
+/* Sleeps for approximately US microseconds.  Interrupts need not
    be turned on.
 
    Busy waiting wastes CPU cycles, and busy waiting with
@@ -153,7 +146,7 @@ timer_udelay (int64_t us)
   real_time_delay (us, 1000 * 1000);
 }
 
-/** Sleeps execution for approximately NS nanoseconds.  Interrupts
+/* Sleeps execution for approximately NS nanoseconds.  Interrupts
    need not be turned on.
 
    Busy waiting wastes CPU cycles, and busy waiting with
@@ -166,23 +159,22 @@ timer_ndelay (int64_t ns)
   real_time_delay (ns, 1000 * 1000 * 1000);
 }
 
-/** Prints timer statistics. */
+/* Prints timer statistics. */
 void
 timer_print_stats (void) 
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
 
-/** Timer interrupt handler. */
+/* Timer interrupt handler. */
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
-  ticks++;  //记录该os已经运行的tick数 
-  thread_tick ();  //更新当前线程从获得CPU到现在所占用的时间
-  check_and_wakeup_sleeping_threads(); //检查是否有线程需要唤醒
+  ticks++;
+  thread_tick ();
 }
 
-/** Returns true if LOOPS iterations waits for more than one timer
+/* Returns true if LOOPS iterations waits for more than one timer
    tick, otherwise false. */
 static bool
 too_many_loops (unsigned loops) 
@@ -201,7 +193,7 @@ too_many_loops (unsigned loops)
   return start != ticks;
 }
 
-/** Iterates through a simple loop LOOPS times, for implementing
+/* Iterates through a simple loop LOOPS times, for implementing
    brief delays.
 
    Marked NO_INLINE because code alignment can significantly
@@ -215,7 +207,7 @@ busy_wait (int64_t loops)
     barrier ();
 }
 
-/** Sleep for approximately NUM/DENOM seconds. */
+/* Sleep for approximately NUM/DENOM seconds. */
 static void
 real_time_sleep (int64_t num, int32_t denom) 
 {
@@ -243,7 +235,7 @@ real_time_sleep (int64_t num, int32_t denom)
     }
 }
 
-/** Busy-wait for approximately NUM/DENOM seconds. */
+/* Busy-wait for approximately NUM/DENOM seconds. */
 static void
 real_time_delay (int64_t num, int32_t denom)
 {
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -4,29 +4,28 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
+#include "synch.h"
 
-/** States in a thread's life cycle. */
+/* States in a thread's life cycle. */
 enum thread_status
   {
-    THREAD_RUNNING,     /**< Running thread. */
-    THREAD_READY,       /**< Not running but ready to run. */
-    THREAD_BLOCKED,     /**< Waiting for an event to trigger. */
-    THREAD_DYING,        /**< About to be destroyed. */
-    THREAD_SLEEP        /**< Sleeping thread. */
+    THREAD_RUNNING,     /* Running thread. */
+    THREAD_READY,       /* Not running but ready to run. */
+    THREAD_BLOCKED,     /* Waiting for an event to trigger. */
+    THREAD_DYING        /* About to be destroyed. */
   };
 
-/** Thread identifier type.
+/* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t) -1)          /**< Error value for tid_t. */
+#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
 
-/** Thread priorities. */
-#define PRI_MIN 0                       /**< Lowest priority. */
-#define PRI_DEFAULT 31                  /**< Default priority. */
-#define PRI_MAX 63                      /**< Highest priority. */
+/* Thread priorities. */
+#define PRI_MIN 0                       /* Lowest priority. */
+#define PRI_DEFAULT 31                  /* Default priority. */
+#define PRI_MAX 63                      /* Highest priority. */
 
-/** A kernel thread or user process.
+/* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
    thread structure itself sits at the very bottom of the page
@@ -76,7 +75,7 @@ typedef int tid_t;
    the `magic' member of the running thread's `struct thread' is
    set to THREAD_MAGIC.  Stack overflow will normally change this
    value, triggering the assertion. */
-/** The `elem' member has a dual purpose.  It can be an element in
+/* The `elem' member has a dual purpose.  It can be an element in
    the run queue (thread.c), or it can be an element in a
    semaphore wait list (synch.c).  It can be used these two ways
    only because they are mutually exclusive: only a thread in the
@@ -85,38 +84,38 @@ typedef int tid_t;
 struct thread
   {
     /* Owned by thread.c. */
-    tid_t tid;                          /**< Thread identifier. */
-    enum thread_status status;          /**< Thread state. */
-    char name[16];                      /**< Name (for debugging purposes). */
-    uint8_t *stack;                     /**< Saved stack pointer. */
-    int priority;                       /**< Priority. */
-    struct list_elem allelem;           /**< List element for all threads list. */
+    tid_t tid;                          /* Thread identifier. */
+    enum thread_status status;          /* Thread state. */
+    char name[16];                      /* Name (for debugging purposes). */
+    uint8_t *stack;                     /* Saved stack pointer. */
+    int priority;                       /* Priority. */
+    struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /**< List element. */
+    struct list_elem elem;              /* List element. */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
-    uint32_t *pagedir;                  /**< Page directory. */
-    struct semaphore sema;             /**< synchronize with child thread */
-    struct thread* parent;            /**<point to parent thread */
-    uint32_t st_exit;                      /**<exit status of the thread */
+    uint32_t *pagedir;                 /* Page directory. */
+    struct list child_process_list;    /* List containing each child process. */
+    int exit_status;                   /* Stores the status upon exit */
+    struct list_elem child_elem;       /* Used to keep track of the element in the child list. */
+    struct semaphore being_waited_on;  /* Used to put a parent thread to sleep when it needs to wait for a child. */
+    struct list file_descriptors;      /* List of file descriptors belonging to this therad. */
+    int cur_fd;                        /* An integer available file descriptor. */
 #endif
 
     /* Owned by thread.c. */
-    unsigned magic;                     /**< Detects stack overflow. */
-    int64_t wake_time;                  /**< The time when the thread should wake up. */
+    unsigned magic;                     /* Detects stack overflow. */
   };
 
-/** If false (default), use round-robin scheduler.
+/* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
    Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
 void thread_init (void);
 void thread_start (void);
-void thread_sleep(int64_t ticks);
-void check_and_wakeup_sleeping_threads(void);
 
 void thread_tick (void);
 void thread_print_stats (void);
@@ -134,7 +133,7 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
-/** Performs some operation on thread t, given auxiliary data AUX. */
+/* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
 
@@ -146,4 +145,4 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-#endif /**< threads/thread.h */
+#endif /* threads/thread.h */
